11c2a1e9e18b4af90ee44ad52fd05c2f3797bdf0
Jason Bohan <jbohan@osv.com>
2022-03-30 10:52:18 -0400

    idk

---
+++ js/app.js
@@ -0,0 +1,37 @@
+"use strict";
+function startGame() {
+    let playerName = getInputValue('playername');
+    logPlayer(playerName);
+    postScore(80, playerName);
+    postScore(-5, playerName);
+}
+function logPlayer(name = 'MultiMath Player') {
+    console.log(`New game starting for player: ${name}`);
+}
+function getInputValue(elementID) {
+    const inputElement = document.getElementById(elementID);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+function postScore(score, playerName = 'MultiMath Player') {
+    let logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+    const scoreElement = document.getElementById('postedScores');
+    scoreElement.innerText = `${score} - ${playerName}`;
+    logger(`Score: ${score}`);
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+const logMessage = (message) => console.log(message);
+function logError(err) {
+    console.error(err);
+}
+//# sourceMappingURL=app.js.map
